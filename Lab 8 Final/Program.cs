98ddf6490423c69d28207200f764c0843a63d30a
Jacky Chan <cjacky624@gmail.com>
2018-10-18 01:27:13 -0400

    Lab8StudentData

---
+++ Lab 8 Final/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8_Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentNames = {"Andrea","Anthony","Brian","Camille","Clayton","Damacious","David","Evan","Heather","Jacky","Johnathan","Katie","Levi","Mauricio","Nicholas","Rudy","SeanO","Steve","Ty"};
+            string[] homeTown = {"Rochester","Saginaw","Flint","Pontiac","Greenville","Detroit","Sterling Heights","Madison Heights","Grand Rapids","Detroit","Lansing","Traverse City","Mackinac City","Ann Arbor","Dearborn","Warren","Muskegon","Livonia","Southfield"};
+            string[] favoriteFood = {"Alfredo","Cheeseburger","Pie","Chicken","Goldfish","Pizza","Apple pie","Frosted Flakes","French fries","Chicken tenders","Pancakes","Sushi","Rice","Pasta","Cake","Brownies","Pho","Ramen","Sushi"};
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter a number between 1 to 19.");
+                    int userInput;
+                    userInput = int.Parse(Console.ReadLine());
+
+                    for (int i = 0; i < studentNames.Length; i++) //
+                    {
+                        if (userInput - 1 == i)
+                        {
+                            Console.WriteLine($"Student {i + 1} is {studentNames[userInput - 1]}");
+
+                            Console.WriteLine($"Would you like to know about {studentNames[userInput - 1]}'s hometown or favorite food? (Type hometown or favorite food)");
+                            string userInput2 = Console.ReadLine().ToLower();
+
+                            if (userInput2 == "hometown")
+                            {
+                                Console.WriteLine($"{studentNames[userInput - 1]}'s hometown is {homeTown[userInput - 1]}");
+
+                            }
+
+                            else if (userInput2 == "favorite food")
+                            {
+
+                                Console.WriteLine($"{studentNames[userInput - 1]}'s favorite food is {favoriteFood[userInput - 1]}");
+                            }
+
+                        }
+
+                    }
+
+                    Console.WriteLine("Would you like to continue?(y/n)");
+                    string userInput3 = Console.ReadLine().ToLower();
+                    if (userInput3 == "n")
+                    {
+                        break;
+                    }
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("The number you entered is out of range. Please enter an integer between 1 to 19.");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter an integer between 1 to 19");
+                }
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Lab 8 Final/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lab 8 Final")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Lab 8 Final")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c1076a3b-61f8-4155-8a99-24b32624425e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
