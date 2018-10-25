6fddc546f6712bc46209518fbf41ef730115c620
Jacky Chan <cjacky624@gmail.com>
2018-10-25 11:10:08 -0400

    Accounted for user input errors

--- Lab 8 Final/Program.cs
+++ Lab 8 Final/Program.cs
@@ -10,41 +10,57 @@ namespace Lab_8_Final
     {
         static void Main(string[] args)
         {
-            string[] studentNames = {"Andrea","Anthony","Brian","Camille","Clayton","Damacious","David","Evan","Heather","Jacky","Johnathan","Katie","Levi","Mauricio","Nicholas","Rudy","SeanO","Steve","Ty"};
-            string[] homeTown = {"Rochester","Saginaw","Flint","Pontiac","Greenville","Detroit","Sterling Heights","Madison Heights","Grand Rapids","Detroit","Lansing","Traverse City","Mackinac City","Ann Arbor","Dearborn","Warren","Muskegon","Livonia","Southfield"};
-            string[] favoriteFood = {"Alfredo","Cheeseburger","Pie","Chicken","Goldfish","Pizza","Apple pie","Frosted Flakes","French fries","Chicken tenders","Pancakes","Sushi","Rice","Pasta","Cake","Brownies","Pho","Ramen","Sushi"};
+            string[] studentNames = { "Andrea", "Anthony", "Brian", "Camille", "Clayton", "Damacious", "David", "Evan", "Heather", "Jacky", "Johnathan", "Katie", "Levi", "Mauricio", "Nicholas", "Rudy", "SeanO", "Steve", "Ty" };
+            string[] homeTown = { "Rochester", "Saginaw", "Flint", "Pontiac", "Greenville", "Detroit", "Sterling Heights", "Madison Heights", "Grand Rapids", "Detroit", "Lansing", "Traverse City", "Mackinac City", "Ann Arbor", "Dearborn", "Warren", "Muskegon", "Livonia", "Southfield" };
+            string[] favoriteFood = { "Alfredo", "Cheeseburger", "Pie", "Chicken", "Goldfish", "Pizza", "Apple pie", "Frosted Flakes", "French fries", "Chicken tenders", "Pancakes", "Sushi", "Rice", "Pasta", "Cake", "Brownies", "Pho", "Ramen", "Sushi" };
 
             while (true)
             {
                 try
                 {
                     Console.WriteLine("Enter a number between 1 to 19.");
-                    int userInput;
-                    userInput = int.Parse(Console.ReadLine());
-
-                    for (int i = 0; i < studentNames.Length; i++) //
+                    int userInput = int.Parse(Console.ReadLine());
+                    if (userInput <= 19 && userInput >= 1)
                     {
-                        if (userInput - 1 == i)
+                        for (int i = 0; i < studentNames.Length; i++)
                         {
-                            Console.WriteLine($"Student {i + 1} is {studentNames[userInput - 1]}");
 
-                            Console.WriteLine($"Would you like to know about {studentNames[userInput - 1]}'s hometown or favorite food? (Type hometown or favorite food)");
-                            string userInput2 = Console.ReadLine().ToLower();
-
-                            if (userInput2 == "hometown")
+                            if (userInput - 1 == i)
                             {
-                                Console.WriteLine($"{studentNames[userInput - 1]}'s hometown is {homeTown[userInput - 1]}");
+                                Console.WriteLine($"Student {i + 1} is {studentNames[userInput - 1]}");
+                                while (true)
+                                {
+                                    Console.WriteLine($"Would you like to know about {studentNames[userInput - 1]}'s hometown or favorite food? (Type hometown or favorite food)");
+                                    string userInput2 = Console.ReadLine().ToLower();
 
-                            }
+                                    if (userInput2 == "hometown") //move down
+                                    {
+                                        Console.WriteLine($"{studentNames[userInput - 1]}'s hometown is {homeTown[userInput - 1]}");
+                                        break;
 
-                            else if (userInput2 == "favorite food")
-                            {
+                                    }
 
-                                Console.WriteLine($"{studentNames[userInput - 1]}'s favorite food is {favoriteFood[userInput - 1]}");
+                                    else if (userInput2 == "favorite food")
+                                    {
+
+                                        Console.WriteLine($"{studentNames[userInput - 1]}'s favorite food is {favoriteFood[userInput - 1]}");
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Invalid input, please try again.");
+                                    }
+                                }
+                                //else do 40
+
                             }
 
                         }
+                    }
 
+                    else
+                    {
+                        Console.WriteLine("Sorry, that is not a valid input. Please enter an integer between 1 to 19.");
                     }
 
                     Console.WriteLine("Would you like to continue?(y/n)");
@@ -57,6 +73,7 @@ namespace Lab_8_Final
                 catch (IndexOutOfRangeException)
                 {
                     Console.WriteLine("The number you entered is out of range. Please enter an integer between 1 to 19.");
+                    
                 }
                 catch (FormatException)
                 {
